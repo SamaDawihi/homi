be476436446b0a6b4546569fe514d5560a6209aa
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-08-30 04:41:56 +0000

    Updating to latest FlutterFlow output.

---
+++ android/app/src/main/kotlin/com/example/my_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.dev.homi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/backend/firebase/firebase_config.dart
@@ -0,0 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
+
+Future initFirebase() async {
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: FirebaseOptions(
+            apiKey: "AIzaSyCZUO4itRIBg09o0pt2Tkoti5-bqLp4nuc",
+            authDomain: "homi-pro.firebaseapp.com",
+            projectId: "homi-pro",
+            storageBucket: "homi-pro.appspot.com",
+            messagingSenderId: "584197013310",
+            appId: "1:584197013310:web:78e178ba9f21663d05b66f"));
+  } else {
+    await Firebase.initializeApp();
+  }
+}
