ee9a236f73f9fbb45f8cfe8536ef76ad83403166
Rana Alfaiyez <alfizerana@gmail.com>
2023-09-15 15:20:29 +0300

    remove debug , add app icon

--- lib/main.dart
+++ lib/main.dart
@@ -88,6 +88,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
       title: 'Homi',
       localizationsDelegates: [
         FFLocalizationsDelegate(),
