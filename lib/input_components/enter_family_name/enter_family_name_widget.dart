5375ab0116c0771b5dba74ab73d63f98e2428d62
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-09-15 16:29:40 +0000

    Updating to latest FlutterFlow output.

--- lib/input_components/enter_family_name/enter_family_name_widget.dart
+++ lib/input_components/enter_family_name/enter_family_name_widget.dart
@@ -218,6 +218,7 @@ class _EnterFamilyNameWidgetState extends State<EnterFamilyNameWidget>
                             24.0, 24.0, 20.0, 24.0),
                       ),
                       style: FlutterFlowTheme.of(context).bodyMedium,
+                      maxLength: 20,
                       keyboardType: TextInputType.emailAddress,
                       cursorColor: FlutterFlowTheme.of(context).primary,
                       validator: _model.familynameControllerValidator
